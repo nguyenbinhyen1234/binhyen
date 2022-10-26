7dae85206e624507494c4447aab07c5f28e381be
nguyenbinhyen1234 <40941588+nguyenbinhyen1234@users.noreply.github.com>
2022-10-26 21:27:48 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,2 @@
-fetch('https://webhook.site/6c90eef6-1fc5-43f3-b05b-800a1c1fcc21')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+fetch('https://webhook.site/6c90eef6-1fc5-43f3-b05b-800a1c1fcc21');
+
